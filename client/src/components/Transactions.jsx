2c47070bccc215461eb262966fdbeac4ef732de6
shubham863 <75516551+shubham863@users.noreply.github.com>
2022-10-30 15:46:48 +0530

    Update Transactions.jsx

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -18,7 +18,7 @@ const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword,
       min-w-full
       flex-col p-3 rounded-md hover:shadow-2xl"
     >
-      <div className="flex flex-col items-center w-full mt-3">
+      {/* <div className="flex flex-col items-center w-full mt-3">
         <div className="display-flex justify-start w-full mb-6 p-2">
           <a href={`https://rinkeby.etherscan.io/address/${addressFrom}`} target="_blank" rel="noreferrer">
             <p className="text-white text-base">From: {shortenAddress(addressFrom)}</p>
@@ -42,7 +42,7 @@ const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword,
         <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
           <p className="text-[#37c7da] font-bold">{timestamp}</p>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
@@ -52,7 +52,7 @@ const Transactions = () => {
 
   return (
     <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
-      <div className="flex flex-col md:p-12 py-12 px-4">
+      {/* <div className="flex flex-col md:p-12 py-12 px-4">
         {currentAccount ? (
           <h3 className="text-white text-3xl text-center my-2">
             Latest Transactions
@@ -68,7 +68,7 @@ const Transactions = () => {
             <TransactionsCard key={i} {...transaction} />
           ))}
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
